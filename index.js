d1521832a9d3685deb3f9b390b7e9ede11b903b1
Nicolas Schneider <nicolas.schneider@epitech.eu>
2023-04-06 21:15:08 -0400

    feat: send email with AWS SES

---
+++ index.js
@@ -0,0 +1,51 @@
+const aws = require("aws-sdk");
+
+exports.handler = async (event) => {
+  const { senderEmail, senderName, message } = JSON.parse(event.body);
+
+  const options = {
+    Source: "nicolas.j.sch@gmail.com",
+    Destination: {
+      ToAddresses: ["nicolas.j.sch@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: "<div>" + message + "</div>",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "hello world",
+      },
+    },
+  };
+
+  try {
+    const ses = new aws.SES({ region: "eu-west-1", apiVersion: "2010-12-01" });
+    const sendEmailResult = await ses.sendEmail(options).promise();
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Email sent successfully!",
+        result: sendEmailResult,
+      }),
+    };
+
+    return response;
+  } catch (error) {
+    console.error("Error sending email:", error);
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error sending email",
+        error: error.message,
+      }),
+    };
+
+    return response;
+  }
+};
